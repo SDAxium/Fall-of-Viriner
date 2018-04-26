fea92cd3461fbb9062016ad46f8db45139b506dd
Mamadou Diallo <Mamadou Diallo@Xarin>
2018-04-25 21:43:30 -0400

    The struggle begins

---
+++ Fall of Viriner/src/Character.java
@@ -0,0 +1,21 @@
+//Trial 1 of many. Sisiame hasn't come to roast my retarded ass yet
+public class Character {
+	public Character()
+	{
+		int Level;
+		int Experience;
+		
+		int Health;
+		int Attack;
+		int Defense;
+		int Magic;
+		int Resistance;
+		int Agility;
+		int Speed;
+		int Stamina;
+		int ManaControl;
+		int Luck;
+		int Intelligence; 
+	}
+
+}
---
+++ Fall of Viriner/src/Warrior.java
@@ -0,0 +1,18 @@
+
+public class Warrior extends Character
+{
+	
+	this.Health = 10;
+	this.Attack = 7;
+	this.Defense = 7;
+	this.Magic = 3;
+	this.Resistance = 7;
+	this.Agility = 2;
+	this.Speed = 3;
+	this.Stamina = 10;
+	this.ManaControl = 6;
+	this.Luck = 1;
+	this.Intelligence = 10;
+	
+}
+
