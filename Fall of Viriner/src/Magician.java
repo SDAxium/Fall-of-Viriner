499fc8061ef29bfddf41301979772de466265820
Mamadou Diallo <Mamadou Diallo@Xarin>
2018-05-11 10:16:59 -0400

    ......

--- Fall of Viriner/src/Magician.java
+++ Fall of Viriner/src/Magician.java
@@ -5,10 +5,6 @@ public class Magician extends Character
 	{	
 		super(
 				charName,7,2,5,10,7,3,3,10,10,3,10
-<<<<<<< HEAD
 			  );
-=======
-			);
->>>>>>> branch 'master' of https://github.com/SDAxium/Fall-of-Viriner.git
 	}
 }
